4c3b81371ace8abe70623e9823c0aff7db82250b
Kanan Farzali <kanan.farzali@gmail.com>
2024-04-17 01:07:24 +0200

    react-redux-typescript setup

---
+++ src/components/ReposList.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useActions } from "../hooks/useActions";
+
+const ReposList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepos } = useActions();
+  const { data, error, loading } = useTypedSelector((state) => state.repos);
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    searchRepos(term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
+    </div>
+  );
+};
+
+export default ReposList;
---
+++ src/state/reducers/reposReducer.ts
@@ -0,0 +1,44 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface ReposState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const initialState = {
+  loading: false,
+  error: null,
+  data: [],
+};
+
+const reducer = (
+  state: ReposState = initialState,
+  action: Action
+): ReposState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOS:
+      return {
+        loading: true,
+        error: null,
+        data: [],
+      };
+    case ActionType.SEARCH_REPOS_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+    case ActionType.SEARCH_REPOS_ERROR:
+      return {
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
